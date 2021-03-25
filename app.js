c995b27e12a2f2ec84a5ddbde2e4f037cf32fefa
Ainiall <angela_l.lopez@hotmail.com>
2021-03-25 01:14:03 +0100

    SDI-2021-713-7.2-Express, depuración y variables de entorno.

---
+++ app.js
@@ -0,0 +1,18 @@
+//Modules
+let express = require('express');
+let app = express();
+
+//Variables
+app.set('port',8081);
+
+app.get('/usuarios', function (req,res){
+    res.send('ver usuarios');
+});
+app.get('/canciones', function (req,res){
+    res.send('ver canciones');
+});
+// lanzar server
+app.listen(app.get('port'), function(){
+    console.log('Servidor activo');
+})
+
