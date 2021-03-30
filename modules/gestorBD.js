6e80acd8861e0680a1427b2989e832848e49d0ad
Ainiall <angela_l.lopez@hotmail.com>
2021-03-30 20:23:28 +0200

    SDI-2021-713-8.2-Arquitectura para el acceso a datos.

---
+++ modules/gestorBD.js
@@ -0,0 +1,24 @@
+// OBJETO
+module.exports = {
+    mongo: null, app: null, init: function (app, mongo) {
+        this.mongo = mongo;
+        this.app = app;
+        // funcion asincrona
+    }, insertarCancion: function (cancion, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insertOne(cancion, function (err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    }
+};
